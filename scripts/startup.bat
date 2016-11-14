7c566f32e77b8507da35f14c808a8a051f9462cf
pato <oyb222222@gmail.com>
2016-11-14 23:59:39 +0800

    bug fixes and log config

--- scripts/startup.bat
+++ scripts/startup.bat
@@ -1,3 +1,4 @@
-echo "财务小工具启动中..."
-java -jar ./lib/CwTool-0.0.1-SNAPSHOT.jar
-echo "对账结束."
+@echo off
+echo 财务小工具启动中...
+java -Dfile.encoding=UTF-8 -jar ./lib/CwTool-0.0.1-SNAPSHOT.jar
+echo 对账结束.
--- src/main/java/com/pat/app/cwtool/Launcher.java
+++ src/main/java/com/pat/app/cwtool/Launcher.java
@@ -4,14 +4,12 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Launcher {
-	private static Logger s_logger = LoggerFactory.getLogger(Launcher.class);
+//	private static Logger s_logger = LoggerFactory.getLogger(Launcher.class);
 
 	public static void main(String[] args) throws IOException {
 		String userDir = System.getProperty("user.dir");
@@ -25,6 +23,7 @@ public class Launcher {
 		});
 		if (dataFiles.length == 0) {
 			System.err.println("当前目录找不到财务明细账或银行明细账文件, 请仅放置要处理的明细账文件在当前目录.");
+			return;
 		}
 		String bankSheetPath = null;
 		String financeSheetPath = null;
@@ -38,6 +37,7 @@ public class Launcher {
 
 		if (bankSheetPath == null || financeSheetPath == null) {
 			System.err.println("当前目录找不到财务明细账或银行明细账文件, 请仅放置要处理的明细账文件在当前目录.");
+			return;
 		}
 
 		String[] batchArgs = { "-bankSheetPath=" + bankSheetPath, "-financeSheetPath=" + financeSheetPath };
